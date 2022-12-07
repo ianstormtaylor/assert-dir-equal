aaf81515151713cea175a6c6334750279fc2ee97
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2022-12-08 00:24:26 +0100

    Modernize repository code, fix eslint no-use-before-define, use strictEqual

--- test/index.js
+++ test/index.js
@@ -1,46 +1,36 @@
 
-var assert = require('assert');
-var equal = require('..');
-var resolve = require('path').resolve;
+const { throws, doesNotThrow } = require('assert');
+const equal = require('..');
+const { resolve } = require('path');
 
 describe('assert-dir-equal', function(){
   it('should not throw when two diretories are equivalent', function(){
-    var one = resolve('test/fixtures/same');
-    var two = resolve('test/fixtures/identical');
-    assert.doesNotThrow(function(){
-      equal(one, two);
-    });
+    const one = resolve('test/fixtures/same');
+    const two = resolve('test/fixtures/identical');
+    doesNotThrow(() => equal(one, two));
   });
 
   it('should throw when two directories are different', function(){
-    var one = resolve('test/fixtures/same');
-    var two = resolve('test/fixtures/different-file');
-    assert.throws(function(){
-      equal(one, two);
-    });
+    const one = resolve('test/fixtures/same');
+    const two = resolve('test/fixtures/different-file');
+    throws(() => equal(one, two));
   });
 
   it('should throw for non utf-8 files', function(){
-    var one = resolve('test/fixtures/same');
-    var two = resolve('test/fixtures/different-image');
-    assert.throws(function(){
-      equal(one, two);
-    });
+    const one = resolve('test/fixtures/same');
+    const two = resolve('test/fixtures/different-image');
+    throws(() => equal(one, two));
   });
 
   it('should ignore hidden files by default', function() {
-    var one = resolve('test/fixtures/with-hidden-file');
-    var two = resolve('test/fixtures/just-file');
-    assert.doesNotThrow(function () {
-      equal(one, two);
-    });
+    const one = resolve('test/fixtures/with-hidden-file');
+    const two = resolve('test/fixtures/just-file');
+    doesNotThrow(() => equal(one, two));
   })
 
   it('should allow seeing hidden files', function(){
-    var one = resolve('test/fixtures/with-hidden-file');
-    var two = resolve('test/fixtures/just-file');
-    assert.throws(function(){
-      equal(one, two, {filter: () => true});
-    });
+    const one = resolve('test/fixtures/with-hidden-file');
+    const two = resolve('test/fixtures/just-file');
+    throws(() => equal(one, two, {filter: () => true}));
   });
 });
